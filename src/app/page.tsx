f06a7d319d1dfd0c65a5a0f9dfe7dfc6ef7f7fc1
handarudwiki <handarudwiki04@gmail.com>
2024-12-06 16:53:33 +0700

    feat: slicing single teacher

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-row">
       <p>Test </p>
       <p>test2</p>
     </div>
